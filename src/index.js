e0b72e2baeb4afa3cd012610f8cb17c910227d44
Bogdan <bveretelnik@gmail.com>
2020-10-27 13:43:58 +0200

    onAuthStateChanged to Listen for Login/Register

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import firebase from "./firebase";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 
-const Root = () => (
-  <Router>
+const Root = ({ history }) => {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        history.push("/");
+      }
+    });
+    // eslint-disable-next-line
+  }, []);
+
+  return (
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </Switch>
-  </Router>
-);
+  );
+};
+const RootWithAuth = withRouter(Root);
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
